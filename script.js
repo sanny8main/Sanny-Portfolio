ff1c7751f6d275965143b0186fbaaae80815b313
sanny8main <97823761+sanny8main@users.noreply.github.com>
2026-01-17 15:11:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ form.addEventListener('submit', async (event) => {
     };
 
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbyUfY-8VYw1cxnVUIYLru0cLHp8m7pt9-RdChSOY9zIH-8H5lykV8Xwip5Qnh2xP4Qp/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbwEmUZFFI6CT7t-zvi2wozntSwgM17XSs-IIXvRmOvNXFoR2YMgYc9sKxFfjghucysE/exec', {
             method: 'POST',
             body: JSON.stringify(formData),
         });
@@ -58,6 +58,7 @@ form.addEventListener('submit', async (event) => {
             'An error occurred. Please try again.';
     }
 });
+
 
 
 
